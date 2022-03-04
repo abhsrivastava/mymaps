d0e8dd322d1e154639d0967c9aa53840c7573d58
Abhishek Srivastava <abhishes@gmail.com>
2022-03-04 13:44:44 -0800

    added find and remove methods

--- lib/mymaps.mli
+++ lib/mymaps.mli
@@ -1,3 +1,20 @@
+(** [t] is the type of maps which binds keys of type ['k] to values of type ['v]*)
 type ('k, 'v) t
+
+(** [empty] is the empty map. *)
 val empty : ('k, 'v) t
+
+(** [insert k v m] is the same map as [m], but with an additional 
+    binding from [k] to [v]. If [k] was already bound in [m], that 
+    binding is replaced by the binding to [v] in the new map. *)
 val insert : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+
+(** [find k m] is [Some v] if [k] is bound to [v] in [m] and None if not. *)
+val find : 'k -> ('k, 'v) t -> 'v option
+
+(** [remove k m] is the same map [m], but without any finding for [k]. If [k]
+    does not exist in [m], then m is unchanged. *)
+val remove : 'k -> ('k, 'v) t -> ('k, 'v) t
+
+(** test method which will be deleted later *)
+val sayHello : string
--- test/mymaps_test.ml
+++ test/mymaps_test.ml
@@ -1,6 +1,14 @@
 open OUnit2
 open Mymaps
-let empty_test = "empty has no bindings" >:: (fun _ -> assert_equal [] (empty))
-let mymap_tests = [empty_test]
+
+let hello_test = 
+  "say hello" >:: (fun _ -> assert_equal "Hello World" (sayHello))
+
+let empty_test = 
+  let (result : (int * int) list) = empty in 
+  "empty has no bindings" >:: (fun _ -> assert_equal [] (result))
+
+let mymap_tests = [hello_test]
+
 let suite = "maps suite" >::: mymap_tests
 let _ = run_test_tt_main suite
